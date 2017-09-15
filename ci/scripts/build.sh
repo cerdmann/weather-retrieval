4a9331bc5f85d41d5a0e4764d3e292b4d331542c
Erds <erds@badmoondev.com>
2017-09-14 23:13:14 -0500

    Tweaks to pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -13,9 +13,9 @@ cd git-repo
 dotnet restore
 dotnet publish -r ubuntu.14.04-x64 --configuration Release -f netcoreapp2.0 --output published-app
 
-tar -zcvf weather-retrieval.tar.gz ./published-app
+zip -r weather-retrieval.zip ./published-app/*
 
-ARTIFACT=weather-retrieval.tar.gz
+ARTIFACT=weather-retrieval.zip
 COMMIT=$(git rev-parse HEAD)
 
 echo $ARTIFACT > ../artifact/release_name.txt
