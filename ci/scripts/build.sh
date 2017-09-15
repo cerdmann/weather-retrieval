fdd1c60842fe2f1f9b9f8c652bd05171f19d0c78
Erds <erds@badmoondev.com>
2017-09-14 22:42:51 -0500

    first pass at pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,26 +4,30 @@ set -e
 export TERM=${TERM:-dumb}
 
 echo "=============================================="
-echo "Beginning build of Spring Boot application"
-echo "$(java -version)"
-echo "$(gradle -version)"
+echo "Beginning build of dotnet core application"
+echo "$(dotnet --version)"
 echo "=============================================="
 
 cd git-repo
 
-./gradlew clean build
+dotnet restore
+dotnet publish -r ubuntu.14.04-x64 --configuration Release -f netcoreapp2.0 --output published-app
 
-ARTIFACT=$(cd ./build/libs && ls latlong-retrieval*.jar)
+tar -zcvf weather-retrieval.tar.gz ./published-app
+
+ARTIFACT=weather-retrieval.tar.gz
 COMMIT=$(git rev-parse HEAD)
 
 echo $ARTIFACT > ../artifact/release_name.txt
 echo $(git log --format=%B -n 1 $COMMIT) > ../artifact/release_notes.md
 echo $COMMIT > ../artifact/release_commitish.txt
 
-cp ./build/libs/$ARTIFACT ../artifact
+cp ./$ARTIFACT ../artifact
 cp ./concourse-dev-manifest.yml ../artifact
+cp -R ./published-app ../artifact/
 
 echo "----------------------------------------------"
 echo "Build Complete"
 ls -lah ../artifact
+ls-lah ../artifact/published-app
 echo "----------------------------------------------"
